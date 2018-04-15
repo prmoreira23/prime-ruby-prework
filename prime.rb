db921ca088ddb92f097bdcdc7f77c04e052b000b
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-15 21:34:23 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,15 @@
 # Add  code here!
-def prime?(num)
-end
+def prime?(n)
+    if n <= 1
+        return false
+    elsif n <= 3
+        return true
+    elsif n % 2 == 0 || n % 3 == 0
+        return false
+    end
+    i = 5
+    while i * i <= n do
+        if n mod i = 0 or n mod (i + 2) = 0
+            return false
+        i ← i + 6
+     return true
